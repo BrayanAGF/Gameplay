88b128938d6fbb2525ed49217d1861936289f0bc
Brayan <mrbrayan.bagf@gmail.com>
2023-11-03 15:28:49 -0600

    Build Produccion v1.01

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client'
 import GamePlay from './GamePlay'
 import './index.css'
 import { NextUIProvider } from "@nextui-org/react";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <NextUIProvider>
           <GamePlay />
         </NextUIProvider>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
